0e0c0aa9381e5210ffc058168ca1cee1231b3417
Adams Mohammed <moadams847@gmail.com>
2021-03-02 09:42:07 +0000

    another commit

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
  #
 from django.http import Http404
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 #
 
 # Create your views here.
